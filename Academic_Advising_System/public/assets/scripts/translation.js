bad5b72237e4e2224ce6cee1ff0031126e1cb9e3
Abdullah Salim <abdullah.salim.pt@gmail.com>
2021-08-11 02:26:01 +0300

    added translation to all users' pages on the system

---
+++ Academic_Advising_System/public/assets/scripts/translation.js
@@ -0,0 +1,124 @@
+let navTrans = {
+
+    /* Student Nav */
+    "personal file": { 
+        ar: 'الملف الشخصي',
+    },
+    "contact with advisor": {
+        ar: 'التواصل مع المرشد الأكاديمي',
+    },
+    "search in message list": {
+        ar: "البحث في قائمة الرسائل",
+    },
+    "book an appoinment with advisor": {
+        ar: "حجز موعد لدى المرشد الأكاديمي",
+    },
+    "update subjects degrees": {
+        ar: "تحديث الدرجات للمواد",
+    },
+    "update absence percentage": {
+        ar: "تحديث نسبة الغياب للمواد",
+    },
+    "upload a complaint to academic advising unit": {
+        ar: "تقديم شكوى إلى وحدة الإرشاد الأكاديمي",
+    },
+    "submit absence execuse or exam unattendance": {
+        ar: "التقديم لعذر غياب أو عذر عدم حضور اختبار",
+    },
+    /* Advisor Nav */
+    "edit personal file": {
+        ar: "تعديل ملفي الشخصي",
+    },
+    "my students": {
+        ar: "الطلاب لدي",
+    },
+    "request persentage absence report": {
+        ar: "اطلب تقرير نسبة الغياب",
+    },
+    "request marks and results report": {
+        ar: "اطلب الدرجات والنتائج",
+    },
+    "update reservation schedules": {
+        ar: "تحديث حجز المواعيد",
+    },
+    "confirm sent reservations": {
+        ar: "تأكيد المواعيد المرسلة",
+    },
+    "talk to student": {
+        ar: "المحادثة مع الطالب",
+    },
+    "search for a message": {
+        ar: "ابحث عن محادثة",
+    },
+    "forward execuse to academic advising unit": {
+        ar: "إرسال العذر إلى وحدة الإرشاد الأكاديمي",
+    },
+    /* Advising Unit Nav */
+    "colleage students": {
+        ar: "طلاب الكلية",
+    },
+    "process excuses": {
+        ar: "معالجة الشكاوى",
+    },
+    "add students": {
+        ar: "أضف طلاب",
+    },
+    "add advisors": {
+        ar: "أضف مرشديين أكاديميين",
+    },
+    "assign students to advisors": {
+        ar: "أضف الطلاب إلى المرشديين الأكاديميين",
+    },
+    "forward excuses to dean or vice-dean": {
+        ar: "إرسال الأعذار إلى عميد الكلية أو نائبه",
+    },
+    /* Dean or Vice-dean Nav */
+    "show prevoise execuses": {
+        ar: "أظهر الأعذار القديمة",
+    },
+    "acceptencse of students excuses": {
+        ar: "قبول أعذار الطلاب",
+    },
+    "show complaints": {
+        ar: "أظهر الشكاوى",
+    },
+    "resolve problems between students and advisors": {
+        ar: "معالجة الشكاوى بين الطلاب والمرشديين الأكاديميين",
+    },
+    "registered colleage students": {
+        ar: "طلاب الكلية المسجلون في الإرشاد الأكاديمي",
+    },
+    "": {
+        ar: "",
+    },
+    "": {
+        ar: "",
+    },
+    "": {
+        ar: "",
+    },
+    "": {
+        ar: "",
+    },
+    "": {
+        ar: "",
+    },
+    
+}
+let mainContentTrans = {
+    "Student Page": {
+        ar: "صفحة الطالب",
+    },
+    "Advisor Page": {
+        ar: "صفحة المرشد الأكاديمي",
+    },
+    "Advising Unit Page": {
+        ar: "صفحة وحدة الإرشاد الأكاديمي",
+    },
+    "Dean and Vice-Dean Page": {
+        ar: "صفحة عميد الكلية ونائبه",
+    },
+}
+/**
+ * 
+ */
