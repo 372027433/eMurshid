8dfa4220835292d5ddabb6c14942c605b46b22eb
Abdullah Basalamah <abdullah.salim.pt@gmail.com>
2021-09-13 17:17:51 +0300

    added student schema

---
+++ Academic_Advising_System/models/student.model.js
@@ -0,0 +1,45 @@
+const {Schema} = require('mongoose')
+
+const studentSchema = new Schema({
+    id: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type:String,
+        required: true, 
+    },
+    password: {
+        type:String,
+        required: true,
+    },
+    email : {
+        type: String,
+        required: true,
+    },
+    advisor_id : String ,
+    Faculity_id: String, // should be Faculity IDs which taken from advisingUnit since each advisingUnit will add their students
+    // so once added we will be adding advisingUnitMemberFaculityID and add to student
+    // and advisors
+    status: String , // should be undergraduate
+    major: String,
+    phone: {
+        type: Number,
+        max: [10, 'phone numbers do not have more than 10 digits'],
+    },
+    cgpa: {
+        type: Number,
+        max: [5, 'highest percent'],
+    },
+    
+    // student Personal Information
+    famliy_members_count: Number,
+    permenet_addres: String,
+    present_address: String,
+    reference_person: String,
+    reference_person_phone: Number,
+})
+
+const Students = mongoose.model('students', studentSchema);
+
+module.exports = Students ;
