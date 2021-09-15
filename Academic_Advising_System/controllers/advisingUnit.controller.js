7030a4ab94dbf752d56daf775555ed84556df86b
Abdullah Basalamah <abdullah.salim.pt@gmail.com>
2021-09-15 15:09:26 +0300

    added students successfully to system

--- Academic_Advising_System/controllers/advisingUnit.controller.js
+++ Academic_Advising_System/controllers/advisingUnit.controller.js
@@ -1,13 +1,18 @@
-const generator = require("generate-password");
-const excelReader = require("read-excel-file/node");
-const nodemailer = require("nodemailer");
-const sgTransport = require("nodemailer-sendgrid-transport");
-const path = require("path");
+/// EXTERNAL LIBRARIES
+const generator = require("generate-password")
+const excelReader = require("read-excel-file/node")
+const nodemailer = require("nodemailer")
+const sgTransport = require("nodemailer-sendgrid-transport")
+const bcrypt = require('bcrypt')
 
+/// NODE CORE LIBRARIES
+const path = require("path")
+
+/// DATABASE MODELS
 const Students = require('../models/student.model')
 
-// SENDER EMAIL USED IN SEND_GRID
-const SENDER_EMAIL = 'emurshid.iu@gmail.com';
+// SENDER EMAIL CONFIGURED IN SEND_GRID
+const SENDER_EMAIL = 'emurshid.iu@gmail.com'
 
 let transporter = nodemailer.createTransport(
   sgTransport({
@@ -32,35 +37,47 @@ exports.renderStudentRegisterPage = (req, res) => {
   });
 };
 
+/**
+ * some work need to be done
+ * add 
+ * -FACULITY_ROLE to object
+ * -Handle errors
+ * -inform advisingUnit member that we have finished sending emials and they are successful
+ * -
+ */
 exports.registerStudents = (req, res) => {
 
   let { filename } = req.file;
   let filepath = path.join(__dirname, "..", "uploads", filename);
 
-  let info = excelReader(filepath).then((rows) => {
-    // create a studentRecords here
+  let info = excelReader(filepath).then( async (rows) => {
+    // creating student credential .Phase-1
     let studentRecords = [];
-    // here I should check the structure of the file
 
     if(rows[0][0].toLocaleLowerCase() !== 'students' || rows[0][1].toLocaleLowerCase() !== 'id' ){
+      // checking file structure
       console.log('error')
       throw new Error('file not formatted correctly')
     }
     for (let i = 1; i < rows.length; i++) {
-      let password = generator.generate({
+      let generatedPassword = generator.generate({
         length: 10,
         numbers: true,
       });
+      let hashedPassword = await bcrypt.hash(generatedPassword, 10)
+      console.log("hashedPassword:",hashedPassword)
       studentRecords.push({
         name: rows[i][0],
         id: rows[i][1],
-        password,
+        password: hashedPassword,
+        toBeSentThenDeletedPassword: generatedPassword,
       });
     }
     return studentRecords;
     
   });
   info.then((records) => {
+    // Adding additional info to student object .Phase-2
     if (records.length <= 0) return res.status(400).send("not enouph students");
 
     // create user
@@ -76,71 +93,63 @@ exports.registerStudents = (req, res) => {
     return bulkStudentWrite;
   })
   .then((studentRecords) => {
-
-    let notSentEmails = []
+    // sending email .Phase-3
+    let notSentEmails = [] // used to handle errors with emails that are not sent
 
     studentRecords.forEach((student) => {
-      let {email,password} = student ;
+      let {email,toBeSentThenDeletedPassword} = student ;
 
       const output = `
           <h1>Welcome to Academic Advising In Islamic University</h1>
           <div> <h2>you have been registered at Academic Advising Unit</h2></div>
           <div><h4>email: <b>${email}</b></h4></div>
-          <div><h4>Password: <b>${password}</b></h4></div>
+          <div><h4>Password: <b>${toBeSentThenDeletedPassword}</b></h4></div>
 
           <p> Ignore this message if you don't know what it mean </p>
       `;
       /// transporter
       let mailOptions = {
-        from: `"Abdullah Salim Basalamah" <${SENDER_EMAIL}>`, // sender address 
+        from: `"Academic Advising Unit at Islamic University" <${SENDER_EMAIL}>`, // sender address 
         to: email, // list of receivers
-        subject: "Node mailing service ✔",
+        subject: "Student Registeration in Academic Advising",
         html: output,
       };
       transporter.sendMail(mailOptions, (err, info) => {
         if (err) { 
           // if an is not sent
           notSentEmails.push(email)
-
           console.log(err) 
         } 
-
+        // if(info.message == 'success'){
+        //   console.log('\tSuccessful sending..!')
+        // }
       });
+      delete student.toBeSentThenDeletedPassword
     });
+    // console.log("Not sent emails: ")
+    // console.log(notSentEmails)
     return studentRecords
   })
   .then(result => {
-    console.log(result)
+    // creating students in DB .Phase-4
     return new Promise((resolve, reject) => {
-      console.log(result)
       Students.create(result, (err, data) => {
         if(err) reject( new Error('We could not enter the database') )
-        console.log('if this after, this will not good')
         resolve(data)
-  
       })
     })
   })
-  .then((result) => {
-    console.log('are we sending to database')
+  .then(() => {
+    // redirect to same page .Phase-5
     res.render("advisingUnitPages/registerStudents", {
       layout: "advisingUnit",
     });
   })
 
   info.catch(err => {
+    // Catch errors 
     console.log("============== err ====================")
     console.log(err)
     res.status(400).json({err:"file not formatted correctly"})
   })
-  /**
-   * here student should be added to system
-   * 1- the sheet file should be read using some third party library
-   * 2- create password for each student
-   * 3- add student to DB , if student could not be created this means it's used, should be deleted or reset has password
-   * 4- send the password to student via email <- using third party library
-   */
-
-  // I should add a message here
-  
 };
